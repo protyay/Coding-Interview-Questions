f24bb3ebbb6bec97bc21247172c15577b9870fbe
Protyay Banerjee <protyay91banerjee@gmail.com>
2021-04-23 22:56:37 +0530

    Added more comments

--- heap/KClosesetPoints2Origin_LC973.java
+++ heap/KClosesetPoints2Origin_LC973.java
@@ -3,11 +3,15 @@ import java.util.*;
 public class KClosesetPoints2Origin_LC973 {
     public int[][] kClosest(int[][] points, int k) {
         int[][] res = new int[k][2];
+        // We are using a MAX-HEAP because at every point , we would want to remove the POINT that is FARTHEST from the 
+        // ORIGIN
         Queue<Point> q = new PriorityQueue<>((a, b) -> Integer.compare(b.originDist, a.originDist));
         for (int i = 0; i < points.length; i++) {
             int[] xy = points[i];
             Point p = new Point(xy, calcDist(xy));
             if (q.size() == k) {
+                // This is the the CRUX of the problem.
+                // If the current co-ordinate IS CLOSER to ORIGIN, it IS a better candidate for the result.
                 if (q.peek().originDist > p.originDist) {
                     q.poll();
                     q.add(p);
@@ -15,6 +19,7 @@ public class KClosesetPoints2Origin_LC973 {
             } else
                 q.add(p);
         }
+        // Forming the result
         for (int i = 0; i < k; i++) {
             res[i] = q.poll().xy;
         }
--- heap/KthLargestElement_LC213.java
+++ heap/KthLargestElement_LC213.java
@@ -17,3 +17,19 @@ public class KthLargestElement_LC213 {
     }
 
 }
+/**
+ * Choosing a max-heap or min-heap is often times confusing. 
+ * 
+ * The framework
+ * useful for thinking is to understand what kind of values would violate my
+ * current assumption.
+ * 
+ * Let's say we have elements [2,3] and we are asked to
+ * return always the 2nd LARGEST element in the stream/array. So the
+ * current 2nd largest is 2. The next element we encounter in the stream might
+ * be < or > 2. 
+ * If the number is less than 2, then it doesn't violates
+ * the current assumption SINCE, the K-TH LARGEST element is DEPENDENT ONLY on
+ * the elements LARGER THAN 2. If we do not encounter a better value, then our
+ * assumption holds and the current 2nd LARGEST candidate won't change.
+ */
