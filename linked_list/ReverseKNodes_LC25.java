adc8f60dd71b45a68f4f8e8030bef7e112aa8a6f
protyay banerjee <protyay91banerjee@gmail.com>
2021-04-30 00:12:26 +0530

    Revision problems updated

--- linked_list/ReverseKNodes_LC25.java
+++ linked_list/ReverseKNodes_LC25.java
@@ -1,20 +1,15 @@
 public class ReverseKNodes_LC25 {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null || k == 1)
+        if (head == null || k == 1 || head.next == null)
             return head;
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
+        ListNode dummy = new ListNode(-1, head);
 
         ListNode prev = dummy;
-        ListNode temp = head;
-        // Calculate the linked list length
         int len = 0;
-
-        while (temp != null) {
+        while (head != null) {
+            head = head.next;
             ++len;
-            temp = temp.next;
         }
-        // For each pointer update cycle, we move next by one no
         while (len >= k) {
             ListNode curr = prev.next;
             ListNode second = curr.next;
@@ -32,16 +27,11 @@ public class ReverseKNodes_LC25 {
 }
 /**
  * We maintain three pointers, prev, curr, second In this reversal, we move K
- * -Groups at a time. 
- * We keep curr pointer fixed, and move second one node
- * ahead(Line 22)
- * On completion of a reversal of a group - we simply use prev =
- * curr(Line 26) to setup curr and second invariant for the next group.
+ * -Groups at a time. We keep curr pointer fixed, and move second one node
+ * ahead(Line 22) On completion of a reversal of a group - we simply use prev =
+ * curr(Line 26) to setup curr and second invariant for the next group. *
+ * Clsrification Questions - How to handle scenarios if K > Len AND scenario for
+ * handling elements in a group less than K elements
  * 
- * 
- * Clsrification Questions - How to handle scenarios if K > Len AND scenario for handling elements
- * in a group less than K elements
- * 
- * Video reference - 
- * https://www.youtube.com/watch?v=Of0HPkk3JgI&t=376s
+ * Video reference - https://www.youtube.com/watch?v=Of0HPkk3JgI&t=376s
  */
